775810cdd4d32fccfc660073e0f7fccd315294d2
Paul Lassasseigne <paul@digital-cover.fr>
2024-03-25 19:01:01 +0100

    feat(MarqueeComponent): added component + style + animation

---
+++ src/components/MarqueeComponent.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { onMounted } from 'vue'
+import gsap from 'gsap'
+
+onMounted(() => {
+  animation()
+})
+
+const animation = () => {
+  gsap.fromTo('.marquee__wrapper', {
+    xPercent: 0
+  }, {
+    xPercent: -100,
+    repeat: -1,
+    duration: 15,
+    ease: 'linear'
+  })
+
+}
+</script>
+
+<template>
+  <section class="marquee">
+    <div class="marquee__wrapper">
+      <span class="marquee__text">Compétences</span>
+      <span class="marquee__text">Logiciels</span>
+      <span class="marquee__text">Langages</span>
+      <span class="marquee__text">Outils</span>
+    </div>
+    <div class="marquee__wrapper">
+      <span class="marquee__text">Compétences</span>
+      <span class="marquee__text">Logiciels</span>
+      <span class="marquee__text">Langages</span>
+      <span class="marquee__text">Outils</span>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.marquee {
+  position: relative;
+  background-color: #fae367;
+  overflow: hidden;
+  padding: 40px 0;
+  display: flex;
+
+  &__wrapper {
+    display: flex;
+    width: fit-content;
+  }
+
+  &__text {
+    display: inline-block;
+    font-family: 'Rammetto One', sans-serif;
+    font-size: 64px;
+    text-transform: uppercase;
+    color: white;
+    margin: 0 20px;
+  }
+
+  &__text:nth-child(2n) {
+    color: #fae367;
+    text-shadow: -2px -2px 0 white, 2px -2px 0 white, -2px 2px 0 white, 2px 2px 0 white, 1px 1px 0 white;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MainCover from '../components/MainCover.vue'
 import AboutMe from '../components/AboutMe.vue'
+import MarqueeComponent from '../components/MarqueeComponent.vue'
 </script>
 
 <template>
   <main>
     <MainCover></MainCover>
     <AboutMe></AboutMe>
+    <MarqueeComponent></MarqueeComponent>
   </main>
 </template>
